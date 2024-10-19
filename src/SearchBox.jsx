4f47001d21403e896f68b7ed9688021719c86b0b
mukulaggy <aggymukul@gmail.com>
2024-10-19 11:46:17 +0530

    Add dark mode mukulaggy

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,46 +1,58 @@
 import TextField from '@mui/material/TextField';
 import Button from "@mui/material/Button";
-import {useState} from "react";
-export default function SearchBox({updateInfo}){
+import { useState } from "react";
+import "./SearchBox.css";
+
+export default function SearchBox({ updateInfo, darkMode }) {
     let [city, setCity] = useState("");
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
     const API_KEY = "57e39b434e35e651ba45b102a06fcb1b";
 
-    let getAWeatherInfo = async() => {
-
-            let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
-            let jsonResponse = await response.json();
-            let result = {
-                city: city,
-                temp: jsonResponse.main.temp,
-                tempMin: jsonResponse.main.temp_min,
-                tempMax: jsonResponse.main.temp_max,
-                humidity: jsonResponse.main.humidity,
-                feelsLike: jsonResponse.main.feels_like,
-                weather: jsonResponse.weather[0].description,
-            };
-            console.log(result)
-            return result;
+    let getAWeatherInfo = async () => {
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse = await response.json();
+        let result = {
+            city: city,
+            temp: jsonResponse.main.temp,
+            tempMin: jsonResponse.main.temp_min,
+            tempMax: jsonResponse.main.temp_max,
+            humidity: jsonResponse.main.humidity,
+            feelsLike: jsonResponse.main.feels_like,
+            weather: jsonResponse.weather[0].description,
+        };
+        return result;
     };
+
     let handleChange = (event) => {
         setCity(event.target.value);
     }
 
     let handleSubmit = async (event) => {
-
-            event.preventDefault();
-            console.log(city);
-            setCity("");
-            let newInfo = await getAWeatherInfo();
-            updateInfo(newInfo);
-
+        event.preventDefault();
+        setCity("");
+        let newInfo = await getAWeatherInfo();
+        updateInfo(newInfo);
     }
+
     return (
-        <div>
+        <div className={`search-box ${darkMode ? 'dark' : ''}`}>
             <form onSubmit={handleSubmit}>
-            <TextField id="city" label="City Name" variant="outlined" value={city} onChange={handleChange} required />
-            <br></br><br></br>
-            <Button variant="contained" type="submit" >Search</Button>
+                <TextField
+                    id="city"
+                    label="City Name"
+                    variant="outlined"
+                    value={city}
+                    onChange={handleChange}
+                    required
+                    className="search-input"
+                />
+                <Button 
+                    variant="contained" 
+                    type="submit"
+                    className="search-button"
+                >
+                    Search
+                </Button>
             </form>
         </div>
     );
--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,25 +1,54 @@
 import SearchBox from "./SearchBox.jsx";
 import InfoBox from "./InfoBox.jsx";
-import {useState} from "react";
+import { useState } from "react";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import "./WeatherApp.css";
 
-export default function WeatherApp(){
+export default function WeatherApp() {
     const [weatherInfo, setWeatherInfo] = useState({
         city: "Delhi",
-        feelsLike : 39.23,
+        feelsLike: 39.23,
         humidity: 58,
         temp: 33.17,
         tempMax: 33.17,
         tempMin: 33.17,
         weather: "broken clouds",
     });
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    const theme = createTheme({
+        palette: {
+            mode: darkMode ? 'dark' : 'light',
+        },
+    });
+
     let updateInfo = (newInfo) => {
         setWeatherInfo(newInfo);
     }
+
     return (
-        <div>
-            <h2>Welcome to the WeatherApp</h2>
-            <SearchBox updateInfo = {updateInfo}/>
-            <InfoBox info = {weatherInfo}/>
-        </div>
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className={`weather-container ${darkMode ? 'dark' : ''}`}>
+                <div className="header">
+                    <h2>Welcome to the WeatherApp</h2>
+                    <IconButton 
+                        onClick={() => setDarkMode(!darkMode)} 
+                        color="inherit"
+                        className="theme-toggle"
+                    >
+                        {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+                    </IconButton>
+                </div>
+                <SearchBox updateInfo={updateInfo} darkMode={darkMode} />
+                <InfoBox info={weatherInfo} darkMode={darkMode} />
+            </div>
+        </ThemeProvider>
     );
 }
